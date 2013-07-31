956772a4e368f670e3ab0414bd26a775ba35c618
Michael Mazzola <mazzola70@gmail.com>
2013-07-31 17:26:33 -0400

    Implemented scrolling resizing of the lightbox with just text and a fixed height for the lightbox

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+	$(window).scroll(function() {
+		var depth = $(this).scrollTop();
+		var windowHeight = $(this).height();
+		// Max width = 800px - 40px padding - 4px border-width
+		var lightboxMaxWidth = 756;
+		var lightboxMinWidth = lightboxMaxWidth/3;
+		var slope = (lightboxMinWidth - lightboxMaxWidth) / (2000 - windowHeight);
+		var lightboxWidth = (slope * depth) + lightboxMaxWidth;
 
+		$(".lightbox").width(lightboxWidth);
+	});
+});
