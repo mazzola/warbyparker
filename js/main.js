9922a8b05f4d7f7ea1fd10db4ab839d9d94fb10e
Michael Mazzola <mazzola70@gmail.com>
2013-08-02 16:07:47 -0400

    Implementing image resizing on scroll

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
+		var pageHeight = 2000;
 		var depth = $(this).scrollTop();
 		var windowHeight = $(this).height();
+		/* Lightbox resizing */
 		// Max width = 800px - 40px padding - 4px border-width
 		var lightboxMaxWidth = 756;
 		var lightboxMinWidth = lightboxMaxWidth/3;
-		var slope = (lightboxMinWidth - lightboxMaxWidth) / (2000 - windowHeight);
+		var slope = (lightboxMinWidth - lightboxMaxWidth) / (pageHeight - windowHeight);
 		var lightboxWidth = (slope * depth) + lightboxMaxWidth;
-
 		$(".lightbox").width(lightboxWidth);
+
+		/* Image resizing */
+		var imageMaxWidth = 100;
+		var imageMinWidth = imageMaxWidth/3;
+		var imgresizeSlope = (imageMinWidth - imageMaxWidth) / (pageHeight - windowHeight);
+		var imageWidth = (imgresizeSlope * depth) + imageMaxWidth;
+		$(".imagelist img").width(imageWidth);
 	});
 });
