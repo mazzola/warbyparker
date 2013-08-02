b07a62cf5eb0a7d67c67f9979ae11051ffe14c5f
Michael Mazzola <mazzola70@gmail.com>
2013-08-02 16:27:33 -0400

    DRYing up resizing code, refactor into it's own function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
 $(document).ready(function() {
 	$(window).scroll(function() {
 		var pageHeight = 2000;
-		var depth = $(this).scrollTop();
-		var windowHeight = $(this).height();
 		/* Lightbox resizing */
-		// Max width = 800px - 40px padding - 4px border-width
-		var lightboxMaxWidth = 756;
+		var lightboxMaxWidth = 756; // Max width = 800px - 40px padding - 4px border-width
 		var lightboxMinWidth = lightboxMaxWidth/3;
-		var slope = (lightboxMinWidth - lightboxMaxWidth) / (pageHeight - windowHeight);
-		var lightboxWidth = (slope * depth) + lightboxMaxWidth;
-		$(".lightbox").width(lightboxWidth);
+		resizeByScroll(".lightbox", lightboxMaxWidth, lightboxMinWidth, pageHeight);
 
 		/* Image resizing */
 		var imageMaxWidth = 100;
 		var imageMinWidth = imageMaxWidth/3;
-		var imgresizeSlope = (imageMinWidth - imageMaxWidth) / (pageHeight - windowHeight);
-		var imageWidth = (imgresizeSlope * depth) + imageMaxWidth;
-		$(".imagelist img").width(imageWidth);
+		resizeByScroll(".imagelist img", imageMaxWidth, imageMinWidth, pageHeight);
 	});
 });
+
+/** Shrinks the specified element linearly based on the current scroll position*/
+function resizeByScroll(element, maxwidth, minwidth, pageHeight) {
+	var depth = $(window).scrollTop();
+	var windowHeight = $(window).height();
+	var slope = (minwidth - maxwidth) / (pageHeight - windowHeight);
+	var newWidth = (slope * depth) + maxwidth;
+	$(element).width(newWidth);
+}
